306fe3672e32af0e84f5eb32607ce15a742e9a99
WilliamPuc01 <william.kn@puccampinas.edu.br>
2026-09-21 15:07:09 -0300

    feat: estrutura inicial do backend com spring boot

---
+++ blog-backend/src/test/java/blog/Blog_backend/BlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.Blog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
